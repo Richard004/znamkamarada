e52b125e8a00f384db32369fafd7a083cbe7c445
Richard Vondráček <richard.vondracek@gmail.com>
2020-01-23 23:49:51 +0100

    Added secrets

--- src/znamkamarada.Services.Library/Services/VignettePersistenceService.cs
+++ src/znamkamarada.Services.Library/Services/VignettePersistenceService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Threading.Tasks;
 using znamkamarada.Services.Library.ModelsDto;
 
 namespace znamkamarada.Services.Library
@@ -6,5 +9,19 @@ namespace znamkamarada.Services.Library
 
     public class VignettePersistenceServiceTableStorage : IVignettePersistenceService
     {
+        private Task<CloudTable> jobStatusTableTask;
+        private string vignetteTableConnectionString = Environment.GetEnvironmentVariable("VIGNETTE-TABLE-CONNECTION-");
+        private string vignetteTableName = "";
+
+
+
+        private CloudTable CreateTable()
+        {
+            var storageAccount = CloudStorageAccount.Parse(vignetteTableConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var tableReference = tableClient.GetTableReference(vignetteTableName);
+            return tableReference;
+        }
+
     }
 }
